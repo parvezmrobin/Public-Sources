46896fa03c819810698f15fc1278c7d2a6820cc1
parvezmrobin <parvezmrobin@users.noreply.github.com>
2016-08-04 21:15:09 +0600

    Update Array Summation.asm
    Added proper commenting.

--- Assembly/Array Summation.asm
+++ Assembly/Array Summation.asm
@@ -1,23 +1,35 @@
+;An Assembly program that shows summtion of an array
+;first integer will be the size of array upto 100
+;next integers will be the elements of array
+;inputs can be separated both by space and new line
 org 100h
 
 .DATA
-a dw 100 dup(?)
-c db 0 
+a dw 100 dup(?) ;an array of word of size 100
+c db 0          ;variable to store size of array
 
 .CODE
 MAIN proc
     call SCAN   ;input size of array
-    mov [c], dl
+    mov [c], dl ;move the size to c
     
     xor bx, bx
     xor cx, cx
     mov cl, [c]
-    INPUTDATA:      ;input array data
+    INPUTDATA:      ;input array data c times
     call SCAN
     mov a[bx], dx
     add bx, 2
-    loop INPUTDATA
+    loop INPUTDATA  ;this will cause iteration cx times
     
+    ;print new line
+    mov ah, 2
+    mov dl, 10
+    int 21h
+    mov dl, 13
+    int 21h
+    
+    ;sum data
     xor dx, dx
     xor bx, bx
     xor cx, cx
@@ -27,14 +39,18 @@ MAIN proc
     add bx, 2
     loop SUM
     
+    ;print summation
     mov ax, dx
     call PRINT
     
     ret
 MAIN endp
 
-
-SCAN proc    
+;a procedure that read an integer value
+;of one or more digit
+;input is terminated by both space and new line
+;the inputed integer will be present in dl
+SCAN proc       
     mov dx, 0
     INPUT:
     mov ah, 1
@@ -55,6 +71,9 @@ SCAN proc
     ret
 SCAN endp
 
+;a procedure that prints an integer
+;consisting of one or more digits
+;the integer must be present in ax
 PRINT proc
     xor cx, cx
     LOOP1:
